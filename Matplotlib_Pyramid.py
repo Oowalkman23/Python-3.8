f40476416d71232807383960dac5bb6195c5f44b
Oowalkman23 <59756043+Oowalkman23@users.noreply.github.com>
2020-01-29 02:42:57 +0700

    Update Matplotlib_Pyramid.py
    Edit comments and rearrange variables.

--- Matplotlib_Pyramid.py
+++ Matplotlib_Pyramid.py
@@ -3,42 +3,39 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def getTop(top,sides,height,p1,delta_h):
-
+    #Recursive process for defining height from outer layer to the core.
     if sides - p1 == 1:
         top[sides-1,p1] = height
-        return top
-    
+        return top    
     else:        
         for x in [p1,sides-1]:
             for y in range(p1,sides):
-                top[x,y] = height
-    
+                top[x,y] = height    
         for x in range(p1,sides):
             for y in [p1,sides-1]:
                 top[x,y] = height
         
         getTop(top,sides-1,height+delta_h,p1+1,delta_h)
 
-
 def printPyramid(sides,height):
-    #Set axes range
+    #Set axes values
     half = sides//2 
     x1 = np.arange(-half,half+1,1)
     y1 = x1.copy()
     x_1, y_1 = np.meshgrid(x1,y1)
     x_2, y_2 = x_1.flatten(), y_1.flatten()
+    
+    #Define top (call function getTop()) and bottom values.
     p1 = 0
     top = y_1.copy()
-
-    #Forming a pyramid shape
     delta_h = height
     getTop(top,sides,height,p1,delta_h)
     top1 = top.flatten()
     bottom = np.zeros_like(top1)
-    #Visualize
     
+    #Visualize 3D Pyramid 
     fig = plt.figure(figsize=(9,6))
-    plot1 = fig.add_subplot(121, projection='3d')
+    plot1 = fig.add_subplot(111, projection='3d')
     plot1.set_title(f'Pyramid sides = {sides}\nHeight = {height}')
     width = depth = 1
 
@@ -46,7 +43,7 @@ def printPyramid(sides,height):
     return plt.show()
 
 def main():
-    #Start
+    #Start.
     print('\nWELCOME TO PYRAMID 3D')
     while True:
         try:
@@ -68,7 +65,7 @@ def main():
             continue
         else:
             break
-
+    #Print the result
     printPyramid(sides,height)
 
 main()
