2d2795902ed51db142b7a89caf5fa5b4bd616ca3
Oowalkman23 <59756043+Oowalkman23@users.noreply.github.com>
2020-01-29 02:34:15 +0700

    Mini program Pyramid 3D with Matplotlib
    Upload pyramid 3d with matplotlib 3d tools, input of sides (odd) and height difference.

---
+++ Matplotlib_Pyramid.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def getTop(top,sides,height,p1,delta_h):
+
+    if sides - p1 == 1:
+        top[sides-1,p1] = height
+        return top
+    
+    else:        
+        for x in [p1,sides-1]:
+            for y in range(p1,sides):
+                top[x,y] = height
+    
+        for x in range(p1,sides):
+            for y in [p1,sides-1]:
+                top[x,y] = height
+        
+        getTop(top,sides-1,height+delta_h,p1+1,delta_h)
+
+
+def printPyramid(sides,height):
+    #Set axes range
+    half = sides//2 
+    x1 = np.arange(-half,half+1,1)
+    y1 = x1.copy()
+    x_1, y_1 = np.meshgrid(x1,y1)
+    x_2, y_2 = x_1.flatten(), y_1.flatten()
+    p1 = 0
+    top = y_1.copy()
+
+    #Forming a pyramid shape
+    delta_h = height
+    getTop(top,sides,height,p1,delta_h)
+    top1 = top.flatten()
+    bottom = np.zeros_like(top1)
+    #Visualize
+    
+    fig = plt.figure(figsize=(9,6))
+    plot1 = fig.add_subplot(121, projection='3d')
+    plot1.set_title(f'Pyramid sides = {sides}\nHeight = {height}')
+    width = depth = 1
+
+    plot1.bar3d(x_2,y_2,bottom,width,depth,top1,shade=True)
+    return plt.show()
+
+def main():
+    #Start
+    print('\nWELCOME TO PYRAMID 3D')
+    while True:
+        try:
+            sides = int(input('\nInput sides (sides=width=length) of your desired pyramid = '))
+        except ValueError:
+            print('\nInvalid input (integer only)!')
+            continue
+        else:
+            if sides % 2 == 0:
+                print('\nInput odd number only!')
+                continue
+            else:
+                break
+    while True:
+        try:
+            height = int(input('\nInput height difference of the pyramid = '))
+        except ValueError:
+            print('\nInvalid input (integer only)!')
+            continue
+        else:
+            break
+
+    printPyramid(sides,height)
+
+main()
