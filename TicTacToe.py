97111ead1cbee86f040b0537781f754644260b5e
Oowalkman23 <59756043+Oowalkman23@users.noreply.github.com>
2020-01-28 15:33:17 +0700

    Update TicTacToe.py
    Edit bugs, and slightly better Bot moves.

--- TicTacToe.py
+++ TicTacToe.py
@@ -29,51 +29,58 @@ def playPlayer(pval, bval):
         except ValueError:
             print('Invalid input type!')
             continue
-
-    board[row-1][column-1] = pval
-            
+    board[row-1][column-1] = pval            
 
 def playBot(pval, bval):
-
     #Bot turn
     while True:
+        Mid = (1,1)
+        Sides = [(0,2), (1,1), (1,2), (2,1)]  
+        
+        if board[1][1] == '?':
+            bot_move = Mid
+        elif [board[x][y].count('?') for x in [0,2] for y in [0,2]] == 0:
+            bot_move = (rd.randint(0,2), rd.randint(0,2)) 
+        else:
+            if sum([board[x][y].count(pval) for x in [0,2] for y in [0,2]]) == 0:
+                bot_move = (rd.choice([0,2]), rd.choice([0,2])) 
+            elif sum([board[x][y].count(pval) for x in [0,2] for y in [0,2]]) == 1:
+                bot_move = (rd.choice([0,2]), rd.choice([0,2])) 
+            elif sum([board[x][y].count(pval) for x in [0,2] for y in [0,2]]) == 2:
+                bot_move = ((rd.choice(Sides))[0], (rd.choice(Sides))[1])            
+            else:
+                bot_move = (rd.randint(0,2), rd.randint(0,2))       
 
-        row = rd.randint(0,2)
-        column = rd.randint(0,2)
-
-        if pval == board[row][column] or bval == board[row][column]:
+        if board[bot_move[0]][bot_move[1]] == pval or board[bot_move[0]][bot_move[1]] == bval:
             continue
         else:
             break
-    
-    board[row][column] = bval
+    board[bot_move[0]][bot_move[1]] = bval
 
-def checkWin(pval, bval):
-
-    # 3 win conditions with 8 total possibility 
+def checkWin(xval):
+    # 3 win conditions with 8 total possibilities     
     win_condition = []
+    w1 = [''.join(board[0])]
+    w2 = [''.join(board[1])]
+    w3 = [''.join(board[2])]
+    w4 = [''.join([board[x][0] for x in range(3)])]
+    w5 = [''.join([board[x][1] for x in range(3)])]
+    w6 = [''.join([board[x][2] for x in range(3)])]
+    w7 = [''.join([board[x][x] for x in range(3)])]
+    w8 = [''.join([board[0][2], board[1][1], board[2][0]])]
+    win_condition.extend(w1+w2+w3+w4+w5+w6+w7+w8)
 
-    cond_1 = []
-    for i in range(3):
-        cond_1.append(''.join(board[i]))
-
-    cond_2 = []
-    for i in range(3):
-        win2 = ''.join([board[x][i] for x in range(3)])
-        cond_2.append(win2)
-
-    cond_3 = []
-    cond_3.append(''.join([board[x][x] for x in range(3)]))
-    cond_3.append(''.join([board[0][2], board[1][1], board[2][0]]))
-
-    win_condition.extend(cond_1 + cond_2 + cond_3)
-
-    if pval*3 in win_condition:
-        return 0
-    elif bval*3 in win_condition:
-        return 1
+    if xval*3 in win_condition:
+        return True
     else:
-        return 2
+        return False
+
+def printBoard():
+    for i in range(3):
+        for j in range(3):
+            print(board[i][j], end='')
+        print()            
+    print()
 
 def main():
     play = 1
@@ -101,67 +108,57 @@ def main():
 
         global board
         board = [['?','?','?'],['?','?','?'],['?','?','?']]
-
-        while True:
-            
-            #Looping for the main game
+        result = 0        
+        
+        while True:            
+            #Print board
             print('\nBoard\nYou Play =', you.val, '\n')
-            for i in range(3):
-                for j in range(3):
-                    print(board[i][j], end='')
-                print()
-            
-            print()
+            printBoard()
             
             #Turn
             if you.pl == 1:
                 playPlayer(you.val, bot.val)
                 you.pl += 1
+                if checkWin(you.val):
+                    result += 1               
+                    break   
             else:
                 playBot(you.val, bot.val)
                 you.pl -= 1
-            
-            #Check win condition
-            result = checkWin(you.val, bot.val)
-
-            if result in [0,1]:                
+                if checkWin(bot.val):
+                    result -= 1               
+                    break
+                      
+            hasil = []
+            for i in board:
+                hasil.extend(i)
+            if '?' not in hasil:
                 break
-            else:
-                continue
         
-        if result == 0:
-            print('\nYou win!')
-            for i in range(3):
-                for j in range(3):
-                    print(board[i][j], end='')
-                print()
-
+        if result == 1:
+            print('\nYou win!\n')            
+        elif result == -1:
+            print('\nYou lose the game..\n')
         else:
-            print('\nYou lose the game..')
+            print("\nIt's a tie game!\n")
         
+        printBoard()
+
         #Choice for another game
         while True:
             try:
-                choice = input('\nWould you like another game? (y/n) ').upper()
+                choice = input('Would you like another game? (y/n) ').upper()
             except ValueError:
-                print('Invalid choice input!')
+                print('\nInvalid choice input!')
                 continue
             else:
                 if choice == 'Y':
                     break
                 elif choice == 'N':
-                    print('Exiting program..')
+                    print('\nExiting program..')
                     play -= 1
                     break
                 else:
-                    print('Invalid choice input!')
+                    print('\nInvalid choice input!')
 
-main()       
-        
-        
-
-
-
-
-
-    
+main()   
