0596c7854934b8311331b250ef1b4482276c9c46
Muhammad Naufal <59756043+Oowalkman23@users.noreply.github.com>
2020-02-06 00:27:35 +0700

    Update Lexicograph_Bitmask.py
    Added comments and edit input var.

--- Lexicograph_Bitmask.py
+++ Lexicograph_Bitmask.py
@@ -1,4 +1,5 @@
 def permutations():
+    # call variables
     global running
     global characters
     global bitmask
@@ -6,16 +7,19 @@ def permutations():
         print(''.join(running), end=' ')
     else:
         for i in range(len(characters)):
+            # basically telling if there is a string hasn't been added to the list yet then..
             if ((bitmask>>i)&1) == 0:
                 bitmask |= 1<<i
                 running.append(characters[i])
                 permutations()
+                # reassign bitmask to 0
                 bitmask ^= 1 << i
                 running.pop()
 
-
-'''raw = raw_input()'''
-characters = list('cghmsuv')
+# initial input
+string = input('Input your strings = ')
+characters = list(string)
+# storage for words
 running = []
 bitmask = 0
 permutations()
